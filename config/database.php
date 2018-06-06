e56d5637e085112deb8a2fb890d5eec5cd75826f
Adriel Guerrero <adrielgro@gmail.com>
2018-06-06 14:32:40 -0800

    Nuevos datos de acceso a la base de datos remota

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
 return array(
     "driver"    =>"mysql",
-    "host"      =>"localhost",
-    "user"      =>"root",
-    "pass"      =>"toor888",
-    "database"  =>"ordinario",
+    "host"      =>"192.185.131.128",
+    "user"      =>"comerci8_ordinar",
+    "pass"      =>"toor1337",
+    "database"  =>"comerci8_ordinario",
     "charset"   =>"utf8"
 );
 ?>
