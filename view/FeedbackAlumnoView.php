ab61771e5292c32aaeae461374e1547ca7b00e1f
Fabian Gandarilla Lopez <fgandarilla@uabc.edu.mx>
2018-06-06 17:24:04 -0700

    Added Feedback page

---
+++ view/FeedbackAlumnoView.php
@@ -0,0 +1,32 @@
+<?php require('includes/header.php'); ?>
+
+<div class="container-fluid">
+
+<div class="row">
+    <div class="col-4">
+        <div class="card">
+            <div class="card-body">
+                Profesor
+                <input type="text" class="form-control">
+            </div>
+        </div>
+    </div>
+    <div class="col-4">
+        <div class="card">
+            <div class="card-body">
+                
+            </div>
+        </div>
+    </div>
+    <div class="col-4">
+        <div class="card">
+            <div class="card-body">
+                
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>
+
+<?php require('includes/footer.php'); ?>
