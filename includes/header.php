6074a8ded8d869b21002fb25136ebca37a88c479
Adriel Guerrero <adrielgro@gmail.com>
2018-06-06 23:55:24 -0800

    Documento del proyecto y barra de menu del sitio web actualizados

--- includes/header.php
+++ includes/header.php
@@ -12,7 +12,7 @@
 
   <body>
     <!-- Menu -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <a class="navbar-brand" href="/"><img src="<?=basename('/assets').'/img/banner.png'?>" style="width: 300px; height: 50px;"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
