6a3e6d475fc8cad3e8d6df492169e9eaa33d878c
Adriel Guerrero <adrielgro@gmail.com>
2018-06-06 21:12:05 -0800

    Upload logo

--- includes/header.php
+++ includes/header.php
@@ -13,7 +13,7 @@
   <body>
     <!-- Menu -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a class="navbar-brand" href="#">Titulo</a>
+      <a class="navbar-brand" href="/"><img src="<?=basename('/assets').'/img/banner.png'?>" style="width: 300px; height: 50px;"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -26,25 +26,13 @@
           <li class="nav-item">
             <a class="nav-link" href="?controller=cursos">Curso</a>
           </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Dropdown
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
+          <li class="nav-item">
+            <a class="nav-link" href="?controller=cursos">Feedback</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
+            <a class="nav-link" href="?controller=cursos">Calificaciones</a>
           </li>
         </ul>
-        <!--form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-        </form-->
       </div>
     </nav>
     <!-- End Menu -->
